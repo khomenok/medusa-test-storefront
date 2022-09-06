57491e25ed1ef31e256c99c94a6917bf69db0c04
Vladimir Khomenok <khomenokvladimir@gmail.com>
2022-09-06 16:14:27 +0300

    feat: basic medusajs env

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {MedusaProvider} from "medusa-react";
+import {QueryClient} from "react-query";
+
+// Your react-query's query client config
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 30000,
+      retry: 1,
+    },
+  },
+})
+
+const queryClientProviderProps = {client: queryClient}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MedusaProvider
+      queryClientProviderProps={queryClientProviderProps}
+      baseUrl={process.env.MEDUSA_SERVER || ''}
+    >
+      <div className="p-10">
+        <Component {...pageProps} />
+      </div>
+    </MedusaProvider>
+  )
 }
 
 export default MyApp
---
+++ ui/ProductListCard/index.tsx
@@ -0,0 +1,9 @@
+const ProductListCard = () => {
+  return (
+    <div>
+      Hele I'm product
+    </div>
+  )
+}
+
+export default ProductListCard
